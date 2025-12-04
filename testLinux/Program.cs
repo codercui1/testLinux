d3ba219a5d2e8aab6bc789bf670542dcd8dc7aa6
lllnico <1217208210@qq.com>
2025-12-04 17:46:01 +0800

    Initial commit

---
+++ testLinux/Program.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia;
+using Avalonia.Media;
+using System;
+
+namespace testLinux
+{
+    internal sealed class Program
+    {
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        [STAThread]
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+        {
+            FontManagerOptions options = new();
+            if (OperatingSystem.IsLinux())
+            {
+                options.DefaultFamilyName = "Noto Sans CJK SC";
+            }
+
+            return AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToTrace()
+                .With(options);
+        }
+    }
+}
